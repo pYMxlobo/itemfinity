6270f5196b59ea5aa5a7fca34df62f102a75a29a
pYMxolbo <pymxolboart@gmail.com>
2024-05-14 23:21:52 -0400

    swap to perfbullet start

--- scripts/ouchie.gd
+++ scripts/ouchie.gd
@@ -6,12 +6,12 @@ var damage_allow = true
 
 @export var timer : Timer
 
-func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	if damage_allow == true:
-		Global.health -= 1
-		print(Global.health)
-		damage_allow = false
-		timer.start()
+#func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+#	if damage_allow == true:
+#		Global.health -= 1
+#		print(Global.health)
+#		damage_allow = false
+#		timer.start()
 
 func _on_timer_timeout():
 	damage_allow = true
@@ -21,3 +21,12 @@ func _process(delta):
 		$hitbox.disabled = true
 	else:
 		$hitbox.disabled = false
+
+
+func _on_bullet_hit():
+	print("hit success!")
+	if damage_allow == true:
+		Global.health -= 1
+		print(Global.health)
+		damage_allow = false
+		timer.start()
