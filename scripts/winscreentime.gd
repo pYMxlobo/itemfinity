8dd1655a8f5884433f2fb4ff009b8a59e2e493dc
pYMxolbo <pymxolboart@gmail.com>
2024-05-20 09:43:45 -0400

    final for real this time

--- scripts/winscreentime.gd
+++ scripts/winscreentime.gd
@@ -4,11 +4,13 @@ extends Label
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if Global.speedrun == true:
+		Global.load_ach()
 		show()
-		if Global.time > Global.lowest_time:
-			text = "Time: " + str(Global.time) + "[NEW RECORD!]
+		if Global.time < Global.lowest_time:
+			text = "Time: " + str(Global.time) + " [NEW RECORD!]
 			Old Record: " + str(Global.lowest_time)
 			Global.lowest_time = Global.time
+			Global.save_ach()
 		else:
 			text = "Time: " + str(Global.time) + "
 			Lowest Record: " + str(Global.lowest_time)
